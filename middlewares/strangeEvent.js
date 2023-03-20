92423222c9e47a1c653ef334e6ebf78c836dbd1c
Yodale <yodale@tutanota.com>
2023-03-20 20:42:30 +0100

    Implemented Pushsafer

--- middlewares/strangeEvent.js
+++ middlewares/strangeEvent.js
@@ -1,12 +1,18 @@
 const { createStrangeEvent } = require("../controllers/strangeEvent");
 const accelerometerData = require("../models/accelerometerData");
+const sendPushsaferNotification = require("./pushsafer");
+
+
 
 const checkForStrangeEvent = (id) => {
   accelerometerData
     .findById(id)
     .lean()
     .then((res) => {
-      createStrangeEvent(res);
+      if (res['xValue'] >= 200 || res['yValue'] >= 200) {
+        createStrangeEvent(res);
+        sendPushsaferNotification();
+      }
     })
     .catch((err) => {
       console.log(err);
