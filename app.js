b4adcba80e137f69b59fb77b0f2790520d730a90
Yodale <yodale@tutanota.com>
2023-03-21 11:31:50 +0100

    refactoring

--- app.js
+++ app.js
@@ -25,6 +25,6 @@ app.use(express.json());
 app.use(accelerometerDataRoutes);
 app.use(strangeEventRoutes);
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log(`Server is running`);
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
 });
