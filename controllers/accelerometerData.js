18d83b70e6ca661b5f323ffc7128dcba58c5f2a4
Yodale <yodale@tutanota.com>
2023-03-21 09:45:58 +0100

    refacotring

--- controllers/accelerometerData.js
+++ controllers/accelerometerData.js
@@ -1,15 +1,29 @@
-const checkForStrangeEvent = require("../middlewares/strangeEvent");
 const accelerometerData = require("../models/accelerometerData");
+const strangeEvent = require("../models/strangeEvent");
 
-exports.createAccelerometerData = async (req, res) => {
-  try {
-    const accelerometerDataInstance = new accelerometerData(req.body);
-    const saveResult = await accelerometerDataInstance.save();
-    checkForStrangeEvent(id=saveResult.id);
-    res.status(201).json(saveResult);
-  } catch (error) {
-    res.status(500).json(error);
-  }
+exports.createAccelerometerData = (req, res) => {
+  const accelerometerDataInstance = new accelerometerData(req.body);
+  accelerometerDataInstance
+    .save()
+    .then((obj) => {
+      if (obj.xValue > 200 || obj.yValue > 200) {
+        const strangeEventInstance = new strangeEvent({
+          accelerometerData: obj._id,
+        });
+        strangeEventInstance
+          .save()
+          .then(() =>
+            res.status(201).json({
+              accelerometerData: accelerometerDataInstance,
+              strangeEvent: strangeEventInstance,
+            })
+          )
+          .catch((err) => res.status(500).json(err));
+      } else {
+        res.status(201).json(accelerometerDataInstance);
+      }
+    })
+    .catch((err) => res.status(500).json(err));
 };
 
 exports.getAccelerometerData = async (req, res) => {
--- controllers/strangeEvent.js
+++ controllers/strangeEvent.js
@@ -1,14 +1,5 @@
 const strangeEvent = require("../models/strangeEvent");
 
-exports.createStrangeEvent = (accelerometerData) => {
-  try {
-    const strangeEventInstance = new strangeEvent(accelerometerData);
-    strangeEventInstance.save();
-  } catch (error) {
-    console.log(error);
-  }
-};
-
 exports.getStrangeEvent = async (req, res) => {
   try {
     const allStrangeEvents = await strangeEvent.find();
